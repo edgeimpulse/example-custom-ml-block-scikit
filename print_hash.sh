bc13cd22841081526038430a6fb60637233c9ede
Jan Jongboom <janjongboom@gmail.com>
2023-06-22 10:27:12 +0200

    Add parameters.json file

--- print_hash.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -e
-
-SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
-
-TAG=$(find $SCRIPTPATH/*.py "$SCRIPTPATH/Dockerfile" "$SCRIPTPATH/requirements.txt" -type f | sort | xargs shasum | shasum | awk -F ' ' '{ print $1 }')
-echo $TAG
